89144d2beda8521ec5f17fb3180dd4f22a1007d0
Kacper Zielinski <236698@student.pwr.edu.pl>
2019-01-16 23:32:41 +0100

    Prototype of StationsNameFiller

---
+++ src/main/java/org/wrocnav/utils/xml/StationNameFiller.java
@@ -0,0 +1,13 @@
+package org.wrocnav.utils.xml;
+
+import org.wrocnav.model.Station;
+
+import java.util.List;
+
+public class StationNameFiller {
+
+    public static List<Station> fillStationNames(List<Station> stations) {
+            
+        return null;
+    }
+}
